ddbce16845d2199c7af97198db360b1b50af9209
Adrian Strasser-King <adrian.strassking@gmail.com>
2023-08-02 23:01:31 -0400

    Added final method from VSCode

--- print_hello.py
+++ print_hello.py
@@ -1,8 +1,16 @@
-"""Function that just prints Hello World!"""
+"""Function that just prints Hello World! (Added, Committed & pushed by me.)"""
 
 def print_hello():
     print("Hello World!")
 
-"""Function that just prints hi!"""
+"""Function that just prints hi! Added by *partner* (Working alone so edited 
+raw code on remote directory and pulled)"""
+
 def print_hi():
     print("hi!")
+
+"""Function that just prints Last commit. Goodbye (Added, Committed & pushed by 
+me again)"""
+
+def last_print():
+    print("Last commit. Goodbye")
