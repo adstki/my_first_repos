462fcee844fb2f064973062ca3f13c546bc0e562
adstki <141259439+adstki@users.noreply.github.com>
2023-08-02 22:15:32 -0400

    Update print_hello.py

--- print_hello.py
+++ print_hello.py
@@ -1,4 +1,8 @@
-"""Function that just prints Hello Wrld!"""
+"""Function that just prints Hello World!"""
 
 def print_hello():
-    print("Hhello World!")
+    print("Hello World!")
+
+"""Function that just prints hi!"""
+    def print_hi():
+        print("hi!")
